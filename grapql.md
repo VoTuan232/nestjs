24a19917279e1d37dd46e991ea48c7c3bdb1f47e
Tuan Vo <tuan.vo@sotatek.com>
2020-12-02 16:06:00 +0700

    feat: mutation comment

--- grapql.md
+++ grapql.md
@@ -1,2 +1,36 @@
 ### GraphQL: https://graphql.org/learn/schema/
+
 - String!: non-nullable, graphql luon cung cap gia tri khi truy van truong nay
+
+<!-- 
+mutation {
+	createComment(idea: "1c3f7b4c-6706-4272-adc4-faea003bcf33", comment: "comment 3") {
+    id,
+    comment
+  }
+} -->
+
+<!-- 
+{
+  whoaim {
+    id
+    username
+  }
+} -->
+
+<!-- 
+{
+	comment(id: "d5ffcab3-b3b8-4f32-99a1-e962a09531ce") {
+    id,
+    created,
+    author {
+      id
+      username
+    }
+    idea {
+      id
+      idea
+    }
+  }
+} -->
+
--- idea app/backend/src/comment/comment.module.ts
+++ idea app/backend/src/comment/comment.module.ts
@@ -4,11 +4,12 @@ import { IdeaEntity } from 'src/idea/idea.entity';
 import { UserEntity } from 'src/user/user.entity';
 import { CommentController } from './comment.controller';
 import { CommentEntity } from './comment.entity';
+import { CommentResolver } from './comment.resolver';
 import { CommentService } from './comment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CommentEntity, UserEntity, IdeaEntity])],
   controllers: [CommentController],
-  providers: [CommentService]
+  providers: [CommentService, CommentResolver]
 })
 export class CommentModule {}
